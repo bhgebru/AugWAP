123b3002bcb9a2163a8148ba5003c11f362d88f5
bhgebru <53275136+bhgebru@users.noreply.github.com>
2019-07-27 21:20:56 -0500

    Update lab5_functions.js

--- js/lab5_functions.js
+++ js/lab5_functions.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+"use strict";
 /* runs test to see if expected argument is === to value returned by function2test argument */
 function myFunctionTest(expected, found) {
   if(Array.isArray(expected)) {
@@ -5,10 +7,10 @@ function myFunctionTest(expected, found) {
       return "TEST FAILED.  Expected [" + found  + "] found [" + expected + " ] ";
     }
     if(expected.length != found.length )
-      return "TEST FAILED.  Expected [" + found  + "] found [" + expected + " ] ";
+    {return "TEST FAILED.  Expected [" + found  + "] found [" + expected + " ] ";}
     for(var i=0; i < found.length; i++) {
       if(expected[i] !== found[i])
-        return "TEST FAILED.  Expected [" + found  + "] found [" + expected + " ] ";
+      {return "TEST FAILED.  Expected [" + found  + "] found [" + expected + " ] ";}
     }
     return "TEST SUCCEEDED";
   }
@@ -21,57 +23,60 @@ function myFunctionTest(expected, found) {
 function max(num1, num2) {
   //return ( num1 > num2 ? num1 : num2);
   if (num1 > num2)
-    return num1;
+  {return num1;}
   return num2;
 }
 function maxOfThree(num1, num2, num3) {
   var tempMax= max(num1, num2);
   if(tempMax > num3)
-    return tempMax;
+  {return tempMax;}
   return num3;
 }
 function isVowel(str) {
   if((typeof str !== "string"))
-    return false;
+  {return false;}
   if(str.length > 1)
-    return false;
+  {return false;}
   var vowels= ["a", "e", "i", "o", "u"];
-  return (-1 != vowels.indexOf(str));
+  return (-1 !== vowels.indexOf(str));
 }
 function sum(numbers ) {
   if(! Array.isArray(numbers) )
-    return 0;
+  {return 0;}
   var totalSum = numbers.reduce(function (accumulator, num) {
     return accumulator + num; }, 0);
   return totalSum;
 }
 function multiply(numbers ) {
   if(! Array.isArray(numbers))
-    return 1;
+  {return 1;}
   return getArrayProduct(numbers);
 }
 function reverse(str) {
   var res="";
   for( var i=0; i < str.length; i++ )
-    res = str[i] + res;
+    {res = str[i] + res;}
   return res;
 }
 function findLongestWord(strList){
   if( ! Array.isArray(strList) )
-    return -1;
+    {return -1;}
   var longestLen= strList[0].length;
-  for( var i=1; i < strList.length; i++ )
-    if( strList[i].length > longestLen )
-      longestLen= strList[i].length;
+  for( var i=1; i < strList.length; i++ ){
+    if( strList[i].length > longestLen ){
+      longestLen= strList[i].length;}
+  }
+
   return longestLen;
 }
 function filterLongWords(strArray, minWordLength) {
-  if( ! Array.isArray(strArray))
+  if( ! Array.isArray(strArray)){
     return -1;
+  }
+
   var resArray= strArray.filter(function(elem, i, array) {
     return elem.length >= minWordLength;
   });
-  console.log(resArray.length);
   return resArray;
 }
 function getMultipliedBy10 (numArray) {
@@ -92,14 +97,18 @@ function getArrayProduct(numArray) {
 }
 function areEqualArrays(array1, array2) {
   try{
-    if(! Array.isArray(array1))   return false;
-    if(! Array.isArray(array2))   return false;
-    if(array1.length != array2.length)
+    if(! Array.isArray(array1))   {return false;}
+    if(! Array.isArray(array2))   {return false;}
+    if(array1.length != array2.length){
       return false;
-    for(var i=0; i < array1.length; i++ )
-      if(array1[i] !== array2[i])
+    }
+
+    for(var i=0; i < array1.length; i++ ){
+      if(array1[i] !== array2[i]){
         return false;
-      return true;
+      }
+    }
+    return true;
   }catch(e) {
     return false;
   }
