fbf2109cae6260099738372a1b82bc8c089f03ac
bhgebru <53275136+bhgebru@users.noreply.github.com>
2019-08-05 18:26:37 -0500

    Add files via upload

---
+++ js/maze.js
@@ -0,0 +1,58 @@
+$(function() {
+  $('#start').click(function() {
+    startClicked=true;
+  });
+  $('#end').click(function() {
+    startClicked=false;
+  });
+  let startClicked=false;
+  $(function(){
+    $(".boundary").mouseover(changeBoundary1);
+    $(".boundary").mouseover(changeBoundaryPop);
+    $("#start").mouseover(start);
+  });
+
+  function start(){
+    $("h2").text("Click the \"S\" to begin.");
+
+    $(".boundary").each(function(index, e){
+      e = $(e);
+      e.removeClass("youlose");
+    });
+    $("#maze").mouseover(trace);
+    $("#end").mouseover(end);
+  }
+
+  function trace(){
+    $("#maze").mouseleave(changeBoundary1);
+    $("#maze").mouseleave(changeBoundaryPop);
+  }
+
+  function end(){
+    if(! startClicked) {
+      return;
+    }
+    alert("          you win ):");
+    $("h2").text("you win ):");
+    $("#end").off("mouseover");
+    startClicked=false;
+  }
+
+  function changeBoundaryPop(){
+    if(! startClicked ){
+      return;
+    }
+    alert("          Sorry you lose :(");
+    $("h2").text("Sorry you lose :(");
+    startClicked=false;
+  }
+
+  function changeBoundary1(){
+    $(".boundary").each(function(index, e){
+      e = $(e);
+      e.addClass("youlose");
+    });
+    //reset();
+  }
+
+});
