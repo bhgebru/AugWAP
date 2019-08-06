ffeea679deca4e9027ae0e235f0fdb977d4d6c31
bhgebru <53275136+bhgebru@users.noreply.github.com>
2019-08-05 20:39:35 -0500

    Update maze.js

--- js/maze.js
+++ js/maze.js
@@ -7,12 +7,24 @@ window.onload= function(){
   });
     alert("         STAAAART. Sorry you lose :(");
   let startClicked=false;
-
+/*
     $(".boundary").mouseover(changeBoundary1);
     $(".boundary").mouseover(changeBoundaryPop);
     $("#start").mouseover(start);
- alert("         TEST ONE. Sorry you lose :(");
+ alert("         TEST ONE. Sorry you lose :(");*/
 
+$(function(){
+    //$(document).ready();
+    
+    //console.log("hi");
+    //$("#boundary1").mouseover(changeBoundary1);
+    //$(".youlose").mouseover(changeBoundary1);
+    ////$("#boundery1").mouseleave(changeBoundary1);
+    $(".boundary").mouseover(changeBoundary1);
+    $(".boundary").mouseover(changeBoundaryPop);
+    $("#start").mouseover(start);
+    
+});
   function start(){
     $("h2").text("Click the \"S\" to begin.");
 
